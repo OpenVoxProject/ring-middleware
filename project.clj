4282b8939d743f82a136e125b5cb91187b5c9d04
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-12 22:12:53 +0000

    Version 2.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/ring-middleware "2.1.3"
+(defproject org.openvoxproject/ring-middleware "2.1.4-SNAPSHOT"
 
   ;; Generally, try to keep version pins in :managed-dependencies and the libraries
   ;; this project actually uses in :dependencies, inheriting the version from
